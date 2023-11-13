a42c4209144fb4149fc0a1a821426ada8a0b4de0
Nazia-Karim-Khan-Oishee <naziakarim@iut-dhaka.edu>
2023-11-13 21:40:17 +0600

    hobby

--- SWE_4538_lab3_FileManagement/controllers/auth.controllers.js
+++ SWE_4538_lab3_FileManagement/controllers/auth.controllers.js
@@ -1,21 +1,16 @@
+const User = require("../dataModels/User.model");
 const path = require("path");
 const bcrypt = require("bcrypt");
 const passport = require("passport");
-const initializePassport = require("../config/passport");
-const fs = require("fs");
 
-let users = []; // store the user info here
-const USER_DATA_FILE = path.join(__dirname, "../user_data.json");
+// const users = []; // store the user info here
 
-try {
-  const data = fs.readFileSync(USER_DATA_FILE);
-  users = JSON.parse(data);
-} catch (err) {}
-initializePassport(
-  passport,
-  (email) => users.find((user) => user.email === email),
-  (id) => users.find((user) => user.id === id)
-);
+// const initializePassport = require("../config/passport");
+// initializePassport(
+//   passport,
+//   email => users.find(user => user.email === email),
+//   id => users.find(user => user.id === id)
+//   );
 
 const getLogin = async (req, res) => {
   const filePath = path.join(__dirname, "..", "views", "login.html");
@@ -24,8 +19,8 @@ const getLogin = async (req, res) => {
 
 const postLogin = (req, res, next) => {
   passport.authenticate("local", {
-    successRedirect: "/welcome",
-    failureRedirect: "/register",
+    successRedirect: "/media-pages",
+    failureRedirect: "/login",
     failureFlash: true,
   })(req, res, next);
 };
@@ -36,42 +31,221 @@ const getRegister = async (req, res) => {
 };
 
 const postRegister = async (req, res, next) => {
-  try {
-    const MIN_PASSWORD_LENGTH = 6;
+  // try {
+  //   const hashedPassword = await bcrypt.hash(req.body.password, 10); // req.body.password ==> password should be exact match to register.html name=password,  10:how many time you want to generate hash. it's a standard default value
+  //   users.push({
+  //     id: Date.now().toString(),
+  //     name: req.body.username ,
+  //     email: req.body.email,
+  //     password: hashedPassword,
+  //   });
 
-    const passwordRegex = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[\W_]).{6,}$/;
+  //   res.redirect("/login");
+  // } catch{
+  //   res.redirect("/register");
+  // }
+  // console.log(users); // show the user list
 
-    if (req.body.password) {
-      // Check if req.body.password is strong enough
-      if (
-        req.body.password.length < MIN_PASSWORD_LENGTH ||
-        !passwordRegex.test(req.body.password)
-      ) {
-        const errorMessage =
-          "Password must be at least 6 characters long and strong enough.";
-        console.error(errorMessage);
-        return res.status(400).json({ error: errorMessage });
+  const { email, password } = req.body;
+  const name = req.body.username;
+
+  console.log(name);
+  console.log(email);
+  console.log(password);
+
+  const errors = [];
+  if (!name || !email || !password) {
+    errors.push("All fields are required!");
+  }
+
+  if (errors.length > 0) {
+    res.status(400).json({ error: errors });
+  } else {
+    //Create New User
+    User.findOne({ email: email }).then((user) => {
+      if (user) {
+        errors.push("User already exists with this email!");
+        res.status(400).json({ error: errors });
+      } else {
+        bcrypt.genSalt(10, (err, salt) => {
+          if (err) {
+            errors.push(err);
+            res.status(400).json({ error: errors });
+          } else {
+            bcrypt.hash(password, salt, (err, hash) => {
+              if (err) {
+                errors.push(err);
+                res.status(400).json({ error: errors });
+              } else {
+                const newUser = new User({
+                  name,
+                  email,
+                  password: hash,
+                });
+                newUser
+                  .save()
+                  .then(() => {
+                    res.redirect("/login");
+                  })
+                  .catch(() => {
+                    errors.push("Please try again");
+                    res.status(400).json({ error: errors });
+                  });
+              }
+            });
+          }
+        });
       }
-    } else {
-      // Check if req.body.password exists
-      const errorMessage = "Password must exist.";
-      console.error(errorMessage);
-      return res.status(400).json({ error: errorMessage });
+    });
+  }
+};
+const getProfileInfos = async (req, res) => {
+  try {
+    const users = await User.find().select("-password");
+    res.json(users);
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const updateProfile = async (req, res) => {
+  try {
+    const { name, currentPassword, newPassword, hobby, profession } = req.body;
+    console.log(newPassword);
+
+    const userId = req.user.id;
+    const user = await User.findById(userId);
+    console.log(user);
+
+    // Update the password if provided
+    if (newPassword) {
+      const isPasswordValid = await bcrypt.compare(
+        currentPassword,
+        user.password
+      );
+
+      if (!isPasswordValid) {
+        return res.status(400).json({ error: "Current password is incorrect" });
+      }
+
+      const hashedPassword = await bcrypt.hash(newPassword, 10);
+      user.password = hashedPassword;
     }
 
-    const hashedPassword = await bcrypt.hash(req.body.password, 10);
-    users.push({
-      id: Date.now().toString(),
-      name: req.body.username,
-      email: req.body.email,
-      password: hashedPassword,
-    });
+    // Update the designation if provided
+    if (hobby) {
+      user.hobby = hobby;
+    }
 
-    fs.writeFileSync(USER_DATA_FILE, JSON.stringify(users, null, 2));
-    res.redirect("/login");
+    if (profession) {
+      user.profession = profession;
+    }
+
+    await user.save();
+
+    res.json({ message: "User information updated successfully" });
   } catch (err) {
-    console.error(err);
-    res.status(500).json({ error: "Internal Server Error" });
+    return res.status(500).json({ msg: err.message });
+  }
+};
+
+const deleteProfile = async (req, res) => {
+  try {
+    const profileID = req.params.id;
+    const profileInfo = await User.findById(profileID);
+
+    if (!profileInfo) {
+      return res.status(404).json({ error: "Profile information not found" });
+    }
+
+    await profileInfo.deleteOne({ _id: profileID });
+
+    res.json({ message: "Profile information deleted successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+// const getMediaPage = async (req, res) => {
+//   const filePath = path.join(__dirname, "..", "views", "mediaFiles.html");
+//   res.sendFile(filePath);
+// };
+
+const postProfileImage = async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ message: "No file provided" });
+    }
+    const photo = req.file.filename;
+
+    const userId = req.user.id;
+    const user = await User.findById(userId);
+    console.log(user);
+
+    if (photo) {
+      user.profile_image = photo;
+    }
+    await user.save();
+
+    res.json({ message: "Profile image updated successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const postMultipleImages = async (req, res) => {
+  try {
+    if (!req.files) {
+      return res.status(400).json({ message: "No file provided" });
+    }
+
+    const photo = req.files.map((file) => file.filename);
+
+    const userId = req.user.id;
+    const user = await User.findById(userId);
+
+    if (photo) {
+      user.images = photo;
+    }
+    await user.save();
+
+    res.json({ message: "Multiple images updated successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const getMultipleImages = async (req, res) => {
+  try {
+    const userId = req.user.id;
+    const user = await User.findById(userId);
+    const images = user.images;
+
+    res.json({ images });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
+
+const postAudioFile = async (req, res) => {
+  try {
+    if (!req.file) {
+      return res.status(400).json({ message: "No file provided" });
+    }
+    const audio = req.file.filename;
+
+    const userId = req.user.id;
+    const user = await User.findById(userId);
+    console.log(user);
+
+    if (audio) {
+      user.audio = audio;
+    }
+    await user.save();
+
+    res.json({ message: "Audio updated successfully" });
+  } catch (error) {
+    res.status(500).json({ error: error.message });
   }
 };
 
@@ -80,4 +254,12 @@ module.exports = {
   getRegister,
   postLogin,
   postRegister,
+  getProfileInfos,
+  updateProfile,
+  deleteProfile,
+  postProfileImage,
+  postMultipleImages,
+  getMultipleImages,
+  postAudioFile,
+  getMediaPage,
 };
